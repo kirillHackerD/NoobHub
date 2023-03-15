8c57308d88ec940236ff2974f7c6c17be50306f7
kirillHackerD <127897794+kirillHackerD@users.noreply.github.com>
2023-03-15 12:26:01 +0300

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -93,30 +93,13 @@ dofullbright()
 Light.LightingChanged:Connect(dofullbright)
 end)
 
-local Tab = Window:NewTab("Esp")
+local Tab = Window:NewTab("Scripts")
 -- Подсекция
-local Section = Tab:NewSection("Esp")
+local Section = Tab:NewSection("Scripts")
 	
 	
-	Section:NewButton("Esp", "ButtonInfo", function()
-while wait(0.5) do
-    for i, childrik in ipairs(workspace:GetDescendants()) do
-        if childrik:FindFirstChild("Humanoid") then
-            if not childrik:FindFirstChild("EspBox") then
-                if childrik ~= game.Players.LocalPlayer.Character then
-                    local esp = Instance.new("BoxHandleAdornment",childrik)
-                    esp.Adornee = childrik
-                    esp.ZIndex = 0
-                    esp.Size = Vector3.new(4, 5, 1)
-                    esp.Transparency = 0.65
-                    esp.Color3 = Color3.fromRGB(255,48,48)
-                    esp.AlwaysOnTop = true
-                    esp.Name = "EspBox"
-                end
-            end
-        end
-    end
-end
+Section:NewButton("popdoors", "ButtonInfo", function()
+
 end)
 
 
