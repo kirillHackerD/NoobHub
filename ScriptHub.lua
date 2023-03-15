396327210c7a34f4868fa4c60e646340cc31958e
kirillHackerD <127897794+kirillHackerD@users.noreply.github.com>
2023-03-15 12:44:18 +0300

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -44,8 +44,8 @@ local Section = Tab:NewSection("Cheat")
 
 
 
-Section:NewButton("Fly GUI", "ButtonInfo", function()
-
+Section:NewButton("popdoors", "ButtonInfo", function()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/poopdoors_edited/main/poopdoors_edited.lua"),true))()
 end)
 
 -- Переключатель
@@ -91,27 +91,3 @@ end
 dofullbright()
  
 Light.LightingChanged:Connect(dofullbright)
-end)
-local Tab = Window:NewTab("ESP")
-local Section = Tab:NewSection("ESP")
-
-Section:NewButton("On", "ButtonInfo", function()
-while wait(0.5) do
-    for i, childrik in ipairs(workspace:GetDescendants()) do
-        if childrik:FindFirstChild("Humanoid") then
-            if not childrik:FindFirstChild("EspBox") then
-                if childrik ~= game.Players.LocalPlayer.Character then
-                    local esp = Instance.new("BoxHandleAdornment",childrik)
-                    esp.Adornee = childrik
-                    esp.ZIndex = 0
-                    esp.Size = Vector3.new(4, 5, 1)
-                    esp.Transparency = 0.65
-                    esp.Color3 = Color3.fromRGB(255,48,48)
-                    esp.AlwaysOnTop = true
-                    esp.Name = "EspBox"
-                end
-            end
-        end
-    end
-end		
-end)		
